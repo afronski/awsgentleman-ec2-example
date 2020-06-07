6b3e86030209baabe7b0a333a0f6f380fceecd2e
Wojciech Gawroński <wojciech.gawronski@pattern-match.com>
2020-06-07 21:44:20 +0200

    Adding proper service and user-data script.

--- user-data.sh
+++ user-data.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
 yum update -y
-amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
-yum install -y httpd mariadb-server
-systemctl start httpd
-systemctl enable httpd
-usermod -a -G apache ec2-user
-chown -R ec2-user:apache /var/www
-chmod 2775 /var/www
-find /var/www -type d -exec chmod 2775 {} \;
-find /var/www -type f -exec chmod 0664 {} \;
-echo "<?php phpinfo(); ?>" > /var/www/html/phpinfo.php
+yum install java
+
+aws s3 cp s3://awsgentleman-jar-files/awsgentleman-ec2-example.jar /home/ec2-user/awsgentleman-ec2-example.jar
+
+chown ec2-user:ec2-user /home/ec2-user/awsgentleman-ec2-example.jar
+chmod 500 /home/ec2-user/awsgentleman-ec2-example.jar
+
+cat << EOF > /usr/lib/systemd/system/awsgentleman-ec2-example.service
+[Unit]
+Description=An example Spring Boot application
+After=syslog.target
+
+[Service]
+Environment=RDS_HOSTNAME=
+Environment=RDS_PORT=3306
+Environment=RDS_DB_NAME=journaler_api
+Environment=RDS_USERNAME=root
+Environment=RDS_PASSWORD=
+User=ec2-user
+ExecStart=/usr/bin/java -jar /home/ec2-user/awsgentleman-ec2-example.jar
+SuccessExitStatus=143
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+chmod 744 /usr/lib/systemd/system/awsgentleman-ec2-example.service
+
+service awsgentleman-ec2-example restart
